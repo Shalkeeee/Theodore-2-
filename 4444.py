1c0a717cf28da27e3e73fcfed277e8680ae1302f
Shalkeeee <161607725+Shalkeeee@users.noreply.github.com>
2024-03-07 10:06:10 +0300

    Add files via upload

---
+++ 4444.py
@@ -0,0 +1,18 @@
+import csv
+import random
+
+with open('scientist.txt', encoding='utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter='#'))
+    symparol = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    for row in reader:
+        Sk = row['ScientistName'].split()
+        row['login'] = f'{Sk[0]}_{Sk[1][0]}{Sk[2][0]}'
+        parol = ''
+        for i in range(10):
+            parol += random.choice(symparol)
+        row['parol'] = parol
+
+    with open('scientist_password.csv', 'w', encoding='utf-8', newline='') as file:
+        writer = csv.DictWriter(file, delimiter='#', fieldnames =['ScientistName', 'preparation', 'date', 'components', 'login', 'parol'])
+        writer.writeheader()
+        writer.writerows(reader)
